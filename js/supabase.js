aa132c8b3200430f4c9e93cbc377987ec1d26081
Ahahmad200 <137672392+Ahahmad200@users.noreply.github.com>
2026-08-31 18:15:49 +0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
 
+const SUPABASE_URL = "https://agebzbieovrsyqwqmkbu.supabase.co";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnZWJ6Ymllb3Zyc3lxd3Fta2J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5NDU0NDcsImV4cCI6MjEwMzUyMTQ0N30.t8t1lD7tuS1iXzsU5O-IqjKRpgjG-OGGUoKxQMEBw1c";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
